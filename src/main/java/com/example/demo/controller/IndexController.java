f11c22554d95fb78fb7e1eac9dcf740367087cef
leon matias <roslin.leon.cix@outlook.com>
2020-07-20 10:32:52 -0500

    add cchange

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -8,19 +8,20 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class IndexController {
 
-	@PostMapping("/welocome")
-	@ResponseBody
-	public String welcome(@RequestParam(required = false, name = "param") String[] paramArray) {
-		String msg = "";
-		int i = 0;
-		if (paramArray == null) {
-			msg = "El array esta vacio";
-		} else {
-			for (String paramArrayItem : paramArray) {
-				msg += "param[" + i + "]" + paramArrayItem + "\n";
-			}
-		}
-		return msg;
-	}
+    @PostMapping("/welocome")
+    @ResponseBody
+    public String welcome(@RequestParam(required = false, name = "param") String[] paramArray) {
+        String msg = "";
+        int i = 0;
+        if (paramArray == null) {
+            msg = "El array esta vacio";
+        } else {
+            for (String paramArrayItem : paramArray) {
+            	
+                msg += "param[" + i + "]" + paramArrayItem + "\n";
+            }
+        }
+        return msg;
+    }
 
 }
